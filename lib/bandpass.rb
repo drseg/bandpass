173664a147d5f5ae2aa1b5f47ca4ccb398015fa6
Daniel Segall <segalld@gmail.com>
2019-09-25 18:43:50 +0100

    Add error handling, default high and low pass functionality

--- lib/bandpass.rb
+++ lib/bandpass.rb
@@ -1,3 +1,34 @@
 class Bandpass
+  class NoInput < RuntimeError; end
+  class NonIntegerInput < RuntimeError; end
+  class NegativeIntegerInput < RuntimeError; end
 
+  HIGH_PASS_FREQUENCY = 1000
+  LOW_PASS_FREQUENCY = 40
+
+  def filter(input)
+    raise NonIntegerInput if non_integer?(input)
+    raise NegativeIntegerInput if negative_integer?(input)
+    raise NoInput if input.empty?
+
+    input.map do |frequency|
+      if frequency < LOW_PASS_FREQUENCY
+        LOW_PASS_FREQUENCY
+      elsif frequency > HIGH_PASS_FREQUENCY
+        HIGH_PASS_FREQUENCY
+      else
+        frequency
+      end
+    end
+  end
+
+  private
+
+  def negative_integer?(input)
+    input.index(&:negative?)
+  end
+
+  def non_integer?(input)
+    input.index { |value| !value.instance_of? Integer }
+  end
 end
--- spec/bandpass_spec.rb
+++ spec/bandpass_spec.rb
@@ -1,8 +1,48 @@
 require_relative '../lib/bandpass'
 
 describe Bandpass do
+  context 'given invalid input' do
+    context 'given an empty array' do
+      it 'raises a no input error' do
+        expect { subject.filter([]) }.to raise_error Bandpass::NoInput
+      end
+    end
+
+    context 'given nil' do
+      it 'raises a no input error' do
+        expect { subject.filter([])}.to raise_error Bandpass::NoInput
+      end
+    end
+
+    context 'given array containing non-integers' do
+      it 'raises a non integer input error' do
+        expect { subject.filter([1, 2, 3, 'x']) }.to raise_error Bandpass::NonIntegerInput
+      end
+    end
+
+    context 'given array containing negative integers' do
+      it 'raises a negative integer input error' do
+        expect { subject.filter([-1, 1, 2]) }.to raise_error Bandpass::NegativeIntegerInput
+      end
+    end
+  end
+
+  context 'given valid input' do
+    context 'given default high pass of 1000' do
+      it 'adjusts high pass frequencies to 1000' do
+        expect(subject.filter([1000, 1001, 2000, 3000])).to eq [1000, 1000, 1000, 1000]
+      end
+
+      context 'given default low pass of 40' do
+        it 'adjusts low pass frequencies to 40' do
+          expect(subject.filter([30])).to eq [40]
+        end
+      end
+    end
+  end
+
   context 'given an input of 20, 30, 40, 100, 500, 1000, 2000' do
-    it 'outputs 40, 40, 40, 100, 500, 1000, 1000' do
+    xit 'outputs 40, 40, 40, 100, 500, 1000, 1000' do
       input = [20, 30, 40, 100, 500, 1000, 2000]
       expected_output = [40, 40, 40, 100, 500, 1000, 1000]
       expect(subject.filter(input)).to eq expected_output
